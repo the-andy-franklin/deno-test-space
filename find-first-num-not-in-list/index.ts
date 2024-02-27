bbdbd41667c6c6a7a50a06232165078bf70f5154
Andy <the.andy.franklin@gmail.com>
2024-02-27 10:47:12 -0800

    comparing arr vs obj solution in find-first-num-not-in-list

--- find-first-num-not-in-list/index.ts
+++ find-first-num-not-in-list/index.ts
@@ -1,19 +1,36 @@
-const input = 100000000;
+const input = 100000;
+
+const arr_start = performance.now();
+
+const arr = Array.from({ length: input }, (_, i) => i);
+
+const arr_answer = (() => {
+	for (let i = 1; true; i++) {
+		if (arr.includes(i)) continue;
+		return i;
+	}
+})();
+
+const arr_end = performance.now();
+
+console.log(arr_answer);
+console.log(`arr solution: ${arr_end - arr_start}ms`);
+
+const obj_start = performance.now();
+
 const obj: { [key: number]: true } = {};
 for (let i = 0; i < input; i++) {
 	obj[i] = true;
 }
 
-const start = performance.now();
-
-const answer = (() => {
+const obj_answer = (() => {
 	for (let i = 1; true; i++) {
 		if (obj[i]) continue;
 		return i;
 	}
 })();
 
-const end = performance.now();
+const obj_end = performance.now();
 
-console.log(answer);
-console.log(`${end - start}ms`);
+console.log(obj_answer);
+console.log(`obj solution: ${obj_end - obj_start}ms`);
