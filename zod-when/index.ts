c2314419ba297985ba90ddd5a1c896128bb342e3
Andy <the.andy.franklin@gmail.com>
2024-03-21 07:39:34 -0700

    getting closer

--- zod-when/index.ts
+++ zod-when/index.ts
@@ -1,46 +1,65 @@
 // deno-lint-ignore-file no-explicit-any
+import { ZodLiteral } from "npm:zod";
+import { ZodObject } from "npm:zod";
+import { ZodEnum } from "npm:zod";
+import { Primitive } from "npm:zod";
 import { z } from "npm:zod";
-import type { ZodEffects, ZodRawShape, ZodSchema, ZodTypeAny } from "npm:zod";
+import type { ZodEffects, ZodRawShape, ZodTypeAny } from "npm:zod";
 
 declare module "npm:zod" {
 	interface ZodObject<T extends ZodRawShape> {
-		when<ThenSchema extends z.ZodObject<any>>(
+		when<IsSchema extends (ZodLiteral<Primitive> | ZodEnum<[string, ...string[]]>), ThenSchema extends ZodObject<any>>(
 			conditionField: keyof T,
 			options: {
-				is: ZodSchema;
+				is: IsSchema;
 				then: ThenSchema;
 			},
 		): ZodEffects<
 			ZodTypeAny,
-			{
-				[P in (keyof T | keyof ThenSchema["_output"])]:
-					| (P extends keyof T ? T[P]["_output"] : never)
-					| (P extends keyof ThenSchema["_output"] ? ThenSchema["_output"][P] : never);
-			}
+			| this["_output"]
+			| (
+				& {
+					[K in keyof ThenSchema["_output"]]: ThenSchema["_output"][K];
+				}
+				& {
+					[P in "conditionField"]: IsSchema["_output"];
+				}
+			)
 		>;
 	}
 }
 
-z.ZodObject.prototype.when = function <T extends ZodRawShape, ThenSchema extends z.ZodObject<any>>(
+ZodObject.prototype.when = function <
+	T extends ZodRawShape,
+	IsSchema extends (ZodLiteral<Primitive> | ZodEnum<[string, ...string[]]>),
+	ThenSchema extends ZodObject<any>,
+>(
 	conditionField: keyof T,
-	{ is, then }: { is: ZodSchema; then: ThenSchema },
+	{ is, then }: { is: IsSchema; then: ThenSchema },
 ) {
-	return this.refine((data) => {
+	let newShape;
+
+	return z.preprocess((data: any) => {
 		if (is.safeParse(data[conditionField]).success) {
-			then.parse(data);
+			newShape = Object.assign(
+				{},
+				this.shape,
+				z.object({ [conditionField]: is }).shape,
+				then.shape,
+			);
 		}
 
-		return true;
-	});
+		return data;
+	}, newShape!);
 };
 
 const unknown_data: unknown = {
 	game_mode: "first_to_goal",
-	goal_amount: 10,
+	goal_amount: "10",
 };
 
 const gameSchema = z.object({
-	game_mode: z.enum(["first_to_goal", "cooperative", "most_at_the_end", "daily_goal"]),
+	game_mode: z.enum(["cooperative", "most_at_the_end", "daily_goal"]),
 	goal_amount: z.string(),
 }).when("game_mode", {
 	is: z.literal("first_to_goal"),
